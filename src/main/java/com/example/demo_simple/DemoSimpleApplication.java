e31c874899439435924733b2f10f07bfcce3d33e
Gulhane <girish.gulhane@capgemini.com>
2023-01-30 15:49:46 +0530

    added file

---
+++ src/main/java/com/example/demo_simple/DemoSimpleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo_simple;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_simple/DemoSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
